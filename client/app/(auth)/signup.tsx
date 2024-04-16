6e29852533a621be3901a291a16056c1d8568868
Tran Binh Kha <tbkha.dev@gmail.com>
2024-04-17 05:22:40 +0700

    feat: handle signup ui

--- client/app/(auth)/signup.tsx
+++ client/app/(auth)/signup.tsx
@@ -1,12 +1,13 @@
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import { router } from 'expo-router';
 import { ROUTES } from '@/constants/route';
+import Checkbox from 'expo-checkbox';
 
 const signup = () => {
   return (
     <View className="w-[80%] bg-white rounded-[36px] p-[36px] flex-col items-center">
-      <View className="flex-row bg-[#ECE0D1] rounded-xl">
+      <View className="flex-row bg-[#ECE0D1] rounded-xl mb-[36px]">
         <TouchableOpacity
           className="w-[110px] px-4 py-2"
           onPress={() => router.replace(ROUTES.LOGIN)}
@@ -15,6 +16,33 @@ const signup = () => {
         </TouchableOpacity>
         <TouchableOpacity className="w-[110px] bg-[#967259] px-4 py-2 rounded-xl">
           <Text className="text-center text-[14px] text-white">Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View className="w-full mb-[36px]">
+        <TextInput
+          className="mb-4 px-3 py-4 rounded-xl border border-[#C5C6CC]"
+          placeholder="Full name *"
+        />
+        <TextInput
+          className="mb-4 px-3 py-4 rounded-xl border border-[#C5C6CC]"
+          placeholder="Email/Phone number *"
+        />
+        <TextInput
+          className="mb-4 px-3 py-4 rounded-xl border border-[#C5C6CC]"
+          placeholder="Password *"
+          secureTextEntry
+        />
+        <TextInput
+          className="mb-4 px-3 py-4 rounded-xl border border-[#C5C6CC]"
+          placeholder="Confirm Password *"
+          secureTextEntry
+        />
+      </View>
+
+      <View className="w-full">
+        <TouchableOpacity className="w-full bg-[#967259] py-3 rounded-2xl mb-4">
+          <Text className="text-[14px] text-white text-center">Sign Up</Text>
         </TouchableOpacity>
       </View>
     </View>
