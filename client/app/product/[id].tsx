5c1e99d1de7b0c05d9ec19f8b7bcefdcefff9ec0
Le Vu Ngan Lam <89247468+nganlamforwork@users.noreply.github.com>
2024-05-13 12:17:38 +0700

    feat: cart storage (#38)
    * feat: cart storage
    
    * feat: cart storage

--- client/app/product/[id].tsx
+++ client/app/product/[id].tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Text,
   View,
@@ -10,15 +10,25 @@ import {
   TextInput,
 } from "react-native";
 import Checkbox from "expo-checkbox";
-import RadioButtonGroup, { RadioButtonItem } from "expo-radio-button";
 import { Stack, useLocalSearchParams, useRouter } from "expo-router";
 import { Feather } from "@expo/vector-icons";
-import { ExtraGroups, Product } from "@/type";
+import { Extra, ExtraGroups, Product } from "@/type";
 import { useQuery } from "@tanstack/react-query";
 import { axiosInstance } from "@/config/axios";
+import { useCartStore } from "@/store/cart";
 
+interface CartItem {
+  product: Product;
+  price: number;
+  extras: Extra[];
+  quantity: number;
+  note?: string;
+}
 const ProductDetail = () => {
-  const [text, setText] = useState("");
+  const [note, setNote] = useState("");
+  const [price, setPrice] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+  const [selectedExtras, setSelectedExtras] = useState<string[]>([]);
 
   const router = useRouter();
   const params = useLocalSearchParams();
@@ -41,6 +51,43 @@ const ProductDetail = () => {
   const handleHeartPress = () => {
     console.log("Heart button pressed");
   };
+  const handleExtraSelection = (extraId: string) => {
+    const isSelected = selectedExtras.includes(extraId);
+    if (isSelected) {
+      setSelectedExtras(selectedExtras.filter((id) => id !== extraId));
+    } else {
+      setSelectedExtras([...selectedExtras, extraId]);
+    }
+  };
+  const addToCart = () => {
+    const cartItem: CartItem = {
+      product: product,
+      price: price,
+      extras: selectedExtras.map((id) => {
+        return product.extraGroups
+          .flatMap((group: ExtraGroups) => group.extras)
+          .find((extra: Extra) => extra.id === id) as Extra;
+      }),
+      quantity: quantity, // Default quantity
+      note: note,
+    };
+    console.log(cartItem);
+    useCartStore.getState().addItem(cartItem);
+  };
+
+  useEffect(() => {
+    let totalPrice = product?.price || 0;
+    selectedExtras.forEach((extraId) => {
+      const extra = product?.extraGroups
+        .flatMap((group: ExtraGroups) => group.extras)
+        .find((extra: Extra) => extra.id === extraId);
+      if (extra) {
+        totalPrice += extra.price;
+      }
+    });
+    setPrice(totalPrice * quantity);
+  }, [selectedExtras, product, quantity]);
+
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" />
@@ -91,15 +138,22 @@ const ProductDetail = () => {
             style={{
               padding: 24,
               gap: 24,
-              paddingBottom: 100,
+              paddingBottom: 160,
             }}
           >
             <Text style={styles.productPageTitle}>Description</Text>
             <Text style={{ fontSize: 14 }}>{product?.description}</Text>
             <>
               {product?.extraGroups?.length &&
-                product.extraGroups.map((item) => {
-                  return <ExtraSection key={item.id} extraItem={item} />;
+                product.extraGroups.map((item: ExtraGroups) => {
+                  return (
+                    <ExtraSection
+                      key={item.id}
+                      extraItem={item}
+                      handleExtraSelection={handleExtraSelection}
+                      selectedExtras={selectedExtras}
+                    />
+                  );
                 })}
             </>
             <Text style={styles.productPageTitle}>Additional Notes</Text>
@@ -107,24 +161,35 @@ const ProductDetail = () => {
               style={styles.input}
               multiline={true}
               numberOfLines={4}
-              onChangeText={setText}
-              value={text}
+              onChangeText={setNote}
+              value={note}
               placeholder="Type your note here"
             />
 
-            <QuantitySelector />
+            <QuantitySelector quantity={quantity} setQuantity={setQuantity} />
           </View>
         </ScrollView>
-        <AddToCartButton />
+        <AddToCartButton
+          quantity={quantity}
+          price={price}
+          onPress={addToCart}
+        />
       </View>
     </View>
   );
 };
+interface ExtraSectionProps {
+  extraItem: ExtraGroups;
+  handleExtraSelection: (extraId: string) => void;
+  selectedExtras: string[];
+}
 
-const ExtraSection = ({ extraItem }: { extraItem: ExtraGroups }) => {
-  const radio = extraItem?.min == extraItem?.max;
-
-  const [current, setCurrent] = useState("test");
+const ExtraSection: React.FC<ExtraSectionProps> = ({
+  extraItem,
+  handleExtraSelection,
+  selectedExtras,
+}) => {
+  const radio = extraItem.min == extraItem.max;
   return (
     <View>
       <View className="flex flex-row gap-2 items-end">
@@ -138,76 +203,104 @@ const ExtraSection = ({ extraItem }: { extraItem: ExtraGroups }) => {
             : ` min ${extraItem?.min}, max ${extraItem?.max}`}
         </Text>
       </View>
-      {radio ? (
-        <RadioButtonGroup
-          containerStyle={{
-            marginTop: 12,
-            display: "flex",
-            gap: 8,
-            width: "100%",
-          }}
-          size={20}
-          selected={current}
-          onSelected={(value: string) => setCurrent(value)}
-          radioBackground="green"
-        >
-          {extraItem?.extras?.map((item) => (
-            <RadioButtonItem
-              value={item.id}
-              label={
-                <View className="flex-row justify-between gap-2">
-                  <Text className="font-medium">{item.name}</Text>
-                  <Text className="text-slate-600">${item.price}</Text>
-                </View>
-              }
+
+      <View style={{ marginTop: 12, display: "flex", gap: 8 }}>
+        {extraItem?.extras?.map((item) => (
+          <TouchableOpacity
+            key={item.id}
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginBottom: 4,
+            }}
+            onPress={() => handleExtraSelection(item.id)}
+            disabled={
+              selectedExtras.length >= extraItem.max &&
+              !selectedExtras.includes(item.id)
+            }
+          >
+            <Checkbox
+              value={selectedExtras.includes(item.id)}
+              onValueChange={() => handleExtraSelection(item.id)}
+              style={{ marginRight: 8 }}
+              className="mr-4 w-4 h-4 border border-[#967259] rounded"
+              color="#967259"
             />
-          ))}
-        </RadioButtonGroup>
-      ) : (
-        <View style={{ marginTop: 12, display: "flex", gap: 8 }}>
-          {extraItem?.extras?.map((item) => (
-            <View className="flex-row justify-between ">
-              <View className="flex flex-row gap-2">
-                <Checkbox className="w-4 h-4 border border-[#967259] rounded" />
-                <Text className="font-medium">{item.name}</Text>
-              </View>
-              <Text className="text-slate-600">${item.price}</Text>
-            </View>
-          ))}
-        </View>
-      )}
+            <Text>{item.name}</Text>
+            <Text style={{ marginLeft: "auto" }} className="text-slate-600">
+              ${item.price}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
     </View>
   );
 };
+interface QuantitySelectorProps {
+  quantity: number;
+  setQuantity: React.Dispatch<React.SetStateAction<number>>;
+}
+const QuantitySelector: React.FC<QuantitySelectorProps> = ({
+  quantity,
+  setQuantity,
+}) => {
+  const decreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
 
-const QuantitySelector = () => {
+  const increaseQuantity = () => {
+    setQuantity(quantity + 1);
+  };
   return (
     <View
       className="flex-row items-center"
       style={{ gap: 4, justifyContent: "center" }}
     >
-      <TouchableOpacity className="p-1 rounded-3xl bg-[#ECE0D1] justify-center items-center">
+      <TouchableOpacity
+        className="p-1 rounded-3xl bg-[#ECE0D1] justify-center items-center"
+        onPress={decreaseQuantity}
+      >
         <Feather name="minus" size={20} color="black" />
       </TouchableOpacity>
       <Text
         className="mx-1 text-[14px] text-[#1F2024]"
         style={{ fontSize: 20 }}
       >
-        1
+        {quantity}
       </Text>
-      <TouchableOpacity className="p-1 rounded-3xl bg-[#ECE0D1] justify-center items-center">
+      <TouchableOpacity
+        className="p-1 rounded-3xl bg-[#ECE0D1] justify-center items-center"
+        onPress={increaseQuantity}
+      >
         <Feather name="plus" size={20} color="black" />
       </TouchableOpacity>
     </View>
   );
 };
 
-const AddToCartButton = () => {
+const AddToCartButton = ({
+  price,
+  quantity,
+  onPress,
+}: {
+  price: number;
+  quantity: number;
+  onPress: () => void;
+}) => {
   return (
-    <TouchableOpacity style={styles.addToCartButton}>
-      <Text style={styles.addToCartPrice}>$5.49</Text>
-      <Text style={styles.addToCartText}>Add to Cart</Text>
-    </TouchableOpacity>
+    <View style={styles.addToCartContainer}>
+      <TouchableOpacity style={styles.addToCartButton} onPress={onPress}>
+        <View>
+          <Text className="text-slate-200 mb-1">{quantity} item(s)</Text>
+          <Text style={styles.addToCartPrice}>
+            ${price && price.toFixed(2)}
+          </Text>
+        </View>
+        <Text style={styles.addToCartText}>Add to Cart</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
@@ -267,18 +360,27 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     minHeight: 100,
   },
+  addToCartContainer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    padding: 24,
+    backgroundColor: "white",
+    shadowColor: "#A9A9A9",
+    shadowOffset: { width: 0, height: 0 }, // Dịch chuyển
+    shadowOpacity: 0.5, // Độ trong suốt
+    shadowRadius: 2, // Bán kính blur
+    elevation: 1, // For Android
+  },
   addToCartButton: {
     backgroundColor: "#38220F",
     paddingVertical: 16,
     paddingHorizontal: 24,
     borderRadius: 16,
-    position: "absolute",
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    bottom: 20,
-    left: 24,
-    right: 24,
   },
   addToCartPrice: {
     color: "white",
--- client/components/OrderCard.tsx
+++ client/components/OrderCard.tsx
@@ -1,79 +1,92 @@
-import { Image, ImageProps, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import React, { useState } from 'react';
-import { AntDesign } from '@expo/vector-icons';
+import {
+  Image,
+  ImageProps,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
+import { Extra, Product } from "@/type";
 
 interface OrderCardProps {
+  product: Product;
+  price: number;
+  extras: Extra[];
+  quantity: number;
+  note?: string;
 }
 
 const OrderCard: React.FC<OrderCardProps> = ({
+  product,
+  price,
+  extras,
+  quantity,
 }) => {
-    const [quantity, setQuantity] = useState(1);
+  const increaseQuantity = () => {};
 
-    const increaseQuantity = () => {
-        setQuantity((prevQuantity) => prevQuantity + 1);
-    };
+  const decreaseQuantity = () => {
+    if (quantity > 1) {
+    }
+  };
 
-    const decreaseQuantity = () => {
-        if (quantity > 1) {
-            setQuantity((prevQuantity) => prevQuantity - 1);
-        }
-    };
-
-    const deleteItem = () => {
-        // Add your delete logic here
-    };
-
-    return (
-        <View
-            style={styles.container}
-            className="flex-1 py-3 px-4 flex-row justify-between items-center bg-white rounded-2xl"
-        >
-            <Image
-                src="https://www.starbucks.vn/media/iebnrg1m/hazelnut-macchiato_tcm89-24778_w1024_n.jpg"
-                className="w-[76px] h-[76px] rounded-full mb-2"
-            />
-            <View className="min-h-[110px] flex-col justify-between">
-                <Text className="text-[14px] font-bold text-[#38220F]">Vanilla Chai</Text>
-                <View className="flex-col">
-                    <Text className="text-[10px] text-[#38220F]">Medium Size</Text>
-                    <Text className="text-[10px] text-[#38220F]">Medium Size</Text>
-                    <Text className="text-[10px] text-[#38220F]">Medium Size</Text>
-                </View>
-                <Text className="text-[#967259] font-bold text-[16px]">$27.45</Text>
-            </View>
-            <View className="flex-row items-center">
-                <TouchableOpacity
-                    onPress={decreaseQuantity}
-                    className="w-6 h-6 rounded-3xl bg-[#ECE0D1] justify-center items-center"
-                >
-                    <AntDesign name="minus" size={14} color="black" />
-                </TouchableOpacity>
-                <Text className="mx-1 text-[14px] text-[#1F2024]">{quantity}</Text>
-                <TouchableOpacity
-                    onPress={increaseQuantity}
-                    className="w-6 h-6 rounded-3xl bg-[#ECE0D1] justify-center items-center"
-                >
-                    <AntDesign name="plus" size={14} color="black" />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={deleteItem}
-                    className="ml-3 w-6 h-6 rounded-3xl bg-[#ECE0D1] justify-center items-center"
-                >
-                    <AntDesign name="delete" size={14} color="black" />
-                </TouchableOpacity>
-            </View>
+  return (
+    <>
+      <View
+        style={styles.container}
+        className="flex-1 p-3 flex-row justify-between items-center bg-white rounded-xl gap-2"
+      >
+        <Image
+          source={{
+            uri: product?.imageUrl,
+          }}
+          className="w-[64px] h-[64px] rounded-full"
+        />
+        <View className="flex-col justify-between gap-1">
+          <Text className="text-[14px] font-bold text-[#38220F]">
+            {product?.name}
+          </Text>
+          <View>
+            {extras &&
+              extras.map((item) => (
+                <Text className="text-[10px] text-[#38220F]">{item.name}</Text>
+              ))}
+          </View>
+          <Text className="text-[#967259] font-bold text-[16px]">
+            ${price && price.toFixed(2)}
+          </Text>
         </View>
-    );
+        <View className="flex-row items-center gap-1">
+          <TouchableOpacity
+            onPress={decreaseQuantity}
+            className="w-4 h-4 rounded-3xl bg-[#ECE0D1] justify-center items-center"
+          >
+            <AntDesign name="minus" size={12} color="black" />
+          </TouchableOpacity>
+          <Text className="mx-1 text-[14px] text-[#1F2024] text-center">
+            {quantity}
+          </Text>
+          <TouchableOpacity
+            onPress={increaseQuantity}
+            className="w-4 h-4 rounded-3xl bg-[#ECE0D1] justify-center items-center"
+          >
+            <AntDesign name="plus" size={12} color="black" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
+  );
 };
 
 export default OrderCard;
 
 const styles = StyleSheet.create({
-    container: {
-        shadowColor: '#A9A9A9',
-        shadowOffset: { width: 0, height: 0 }, // Dịch chuyển
-        shadowOpacity: 0.5, // Độ trong suốt
-        shadowRadius: 2, // Bán kính blur
-        elevation: 1, // For Android
-    },
+  container: {
+    shadowColor: "#A9A9A9",
+    shadowOffset: { width: 0, height: 0 }, // Dịch chuyển
+    shadowOpacity: 0.5, // Độ trong suốt
+    shadowRadius: 2, // Bán kính blur
+    elevation: 1, // For Android
+  },
 });
